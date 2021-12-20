8849f301c592a64a29b98d716a28b94e08aad678
Darren Wong <darrenwong@Darrens-MacBook-Air.local>
2021-12-20 16:32:33 +1100

    4.4 commit

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -6,3 +6,84 @@ test("dummy returns one", () => {
 	const result = listHelper.dummy(blogs)
 	expect(result).toBe(1)
 })
+
+describe("total likes", () => {
+	const listWithBlogs = [
+		{
+			_id: "5a422aa71b54a676234d17f8",
+			title: "Go To Statement Considered Harmful",
+			author: "Edsger W. Dijkstra",
+			url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+			likes: 5,
+			__v: 0
+		},
+		{
+			_id: "5a422aasfsfjsf936234d17f8",
+			title: "Deploy a Python Visualization Panel App to Google Cloud",
+			author: "Sophia Yang",
+			url: "https://towardsdatascience.com/deploy-a-python-visualization-panel-app-to-google-cloud-cafe558fe787",
+			likes: 68,
+			__v: 0
+		},
+		{
+			_id: "4424sff424slfflsfll2334",
+			title: "Introduction to SHAP with Python",
+			author: "Connor O'Sullivan",
+			url: "https://towardsdatascience.com/introduction-to-shap-with-python-d27edc23c454",
+			likes: 76,
+			__v: 0
+		},
+		{
+			_id: "5a422a851b54a676234d17f7",
+			title: "React patterns",
+			author: "Michael Chan",
+			url: "https://reactpatterns.com/",
+			likes: 7,
+			__v: 0
+		},
+		{
+			_id: "5a422b3a1b54a676234d17f9",
+			title: "Canonical string reduction",
+			author: "Edsger W. Dijkstra",
+			url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+			likes: 12,
+			__v: 0
+		},
+		{
+			_id: "5a422b891b54a676234d17fa",
+			title: "First class tests",
+			author: "Robert C. Martin",
+			url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+			likes: 10,
+			__v: 0
+		},
+		{
+			_id: "5a422ba71b54a676234d17fb",
+			title: "TDD harms architecture",
+			author: "Robert C. Martin",
+			url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+			likes: 0,
+			__v: 0
+		},
+		{
+			_id: "5a422bc61b54a676234d17fc",
+			title: "Type wars",
+			author: "Robert C. Martin",
+			url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+			likes: 2,
+			__v: 0
+		}  
+	]
+
+	test("if passed one blog, total likes should be just the likes from that", () => {
+		expect(listHelper.totalLikes([listWithBlogs[0]])).toBe(5)
+	})
+
+	test("if no blogs, then total likes should be 0", () => {
+		expect(listHelper.totalLikes([])).toBe(0)
+	})
+
+	test("if passed a larger list, calculation should be right", () => {
+		expect(listHelper.totalLikes(listWithBlogs)).toBe(180)
+	})
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,16 @@
+/* eslint-disable no-unused-vars */
 const dummy = (blogs) => {
 	return 1
 }
-  
+
+const totalLikes = (blogs) => {
+    
+	return blogs.length === 0 ?
+		0 :
+		blogs.reduce((sum, blog) => sum + blog.likes, 0)
+    
+} 
+
 module.exports = {
-	dummy
+	dummy, totalLikes
 }
